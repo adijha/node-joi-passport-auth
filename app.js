4d8a8214d4b480dfaa4a6b6065bcb437f18b73af
Aditya Kumar Jha <adityajha126@gmail.com>
2019-12-11 10:53:32 +0530

    ..

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-
-
 const express = require('express');
 
 const app = express();
-
 
 //ROUTES
 app.get('/', (req, res) => {
 	res.send('We are on home');
 });
 
-//server listen
+//server listenn
 app.listen(3000, () => {
 	console.log('App listening on port 3000!');
 });
